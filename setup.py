d56928865899ff7156adcd25215be5ace73e9dca
Racter <paracetamolboy@gmail.com>
2017-09-07 16:06:31 +0200

    Ran this through 3to2 to be compatible with Python 2.7.

--- setup.py
+++ setup.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python
 # -* encoding: utf-8 *-
+from __future__ import absolute_import
 import os
 from setuptools import setup
+from io import open
 
 HERE = os.path.dirname(__file__)
 
 try:
-    long_description = open(os.path.join(HERE, 'README.rst')).read()
+    long_description = open(os.path.join(HERE, u'README.rst')).read()
 except IOError:
     long_description = None
 
 
 setup(
-    name="django-postgresql-setrole",
-    version="1.0.8",
-    packages=["postgresql_setrole"],
+    name=u"django-postgresql-setrole",
+    version=u"1.0.8",
+    packages=[u"postgresql_setrole"],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: POSIX",
+        u"Development Status :: 5 - Production/Stable",
+        u"Intended Audience :: Developers",
+        u"Intended Audience :: System Administrators",
+        u"Programming Language :: Python :: 3 :: Only",
+        u"License :: OSI Approved :: BSD License",
+        u"Operating System :: POSIX",
     ],
-    url="https://github.com/jdelic/django-postgresql-setrole/",
-    author="Jonas Maurus (@jdelic)",
-    author_email="jonas-postgresql-setrole@gopythongo.com",
-    maintainer="GoPythonGo.com",
-    maintainer_email="info@gopythongo.com",
-    description="Execute SET ROLE on every PostgreSQL connection in the Django ORM",
+    url=u"https://github.com/jdelic/django-postgresql-setrole/",
+    author=u"Jonas Maurus (@jdelic)",
+    author_email=u"jonas-postgresql-setrole@gopythongo.com",
+    maintainer=u"GoPythonGo.com",
+    maintainer_email=u"info@gopythongo.com",
+    description=u"Execute SET ROLE on every PostgreSQL connection in the Django ORM",
     long_description=long_description,
 )
